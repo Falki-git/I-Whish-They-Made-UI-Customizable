8d72d5666c03729e6a4c76d1a5423f239847f1f0
Falki-git <72734856+Falki-git@users.noreply.github.com>
2023-06-28 20:03:36 +0200

    Update postbuild-debug.bat

--- postbuild-debug.bat
+++ postbuild-debug.bat
@@ -1,4 +1,4 @@
-xcopy /y "..\Debug\BepInEx\plugins\CustomizableUI\CustomizableUI.dll" "D:\Steam\steamapps\common\Kerbal Space Program 2 0.1.2.0 DEV\BepInEx\plugins\CustomizableUI"
-xcopy /y "..\Debug\BepInEx\plugins\CustomizableUI\CustomizableUI.pdb" "D:\Steam\steamapps\common\Kerbal Space Program 2 0.1.2.0 DEV\BepInEx\plugins\CustomizableUI"
+xcopy /y "..\Debug\BepInEx\plugins\CustomizableUI\CustomizableUI.dll" "D:\Steam\steamapps\common\Kerbal Space Program 2 0.1.3.0 DEV\BepInEx\plugins\CustomizableUI"
+xcopy /y "..\Debug\BepInEx\plugins\CustomizableUI\CustomizableUI.pdb" "D:\Steam\steamapps\common\Kerbal Space Program 2 0.1.3.0 DEV\BepInEx\plugins\CustomizableUI"
 
-powershell "start-process ""D:\Steam\steamapps\common\Kerbal Space Program 2 0.1.2.0 DEV\KSP2_x64.exe"""
+powershell "start-process ""D:\Steam\steamapps\common\Kerbal Space Program 2 0.1.3.0 DEV\KSP2_x64.exe"""
