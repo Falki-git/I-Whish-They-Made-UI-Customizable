264c2dab9f7010e70e10d1e8fa0fd1f4059be158
Falki <72734856+Falki-git@users.noreply.github.com>
2023-08-30 23:13:23 +0200

    Delete postbuild-debug.bat as postbuild will handle everything now

--- postbuild-debug.bat
+++
@@ -1,4 +0,0 @@
-xcopy /y "..\Debug\BepInEx\plugins\CustomizableUI\CustomizableUI.dll" "D:\Steam\steamapps\common\Kerbal Space Program 2 0.1.3.1 DEV\BepInEx\plugins\CustomizableUI"
-xcopy /y "..\Debug\BepInEx\plugins\CustomizableUI\CustomizableUI.pdb" "D:\Steam\steamapps\common\Kerbal Space Program 2 0.1.3.1 DEV\BepInEx\plugins\CustomizableUI"
-
-powershell "start-process ""D:\Steam\steamapps\common\Kerbal Space Program 2 0.1.3.1 DEV\KSP2_x64.exe"""
