87ab852fe6ad4b46c3ea8aa019e91f98d5511444
Falki-git <72734856+Falki-git@users.noreply.github.com>
2023-12-28 22:58:48 +0100

    Add keybind CTRL+I

--- CustomizableUIProject/CustomizableUIPlugin.cs
+++ CustomizableUIProject/CustomizableUIPlugin.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using KSP.Game;
 using KSP.Messages;
@@ -24,6 +25,10 @@ public class CustomizableUIPlugin : BaseSpaceWarpPlugin
     public static CustomizableUIPlugin Instance { get; set; }
 
     private ManualLogSource _logger = BepInEx.Logging.Logger.CreateLogSource("CustomizableUI");
+    
+    private ConfigEntry<bool> _enableKeybinding;
+    private ConfigEntry<KeyCode> _keybind1;
+    private ConfigEntry<KeyCode> _keybind2;
 
     public override void OnInitialized()
     {
@@ -47,6 +52,7 @@ public class CustomizableUIPlugin : BaseSpaceWarpPlugin
 
         SubscribeToMessages();
         Styles.Initialize();
+        InitializeConfigs();
     }
 
     private void SubscribeToMessages()
@@ -67,5 +73,38 @@ public class CustomizableUIPlugin : BaseSpaceWarpPlugin
         UI.Instance.OnGUI();
     }
 
-    public void Update() => Manager.Instance.Update();
+    public void Update()
+    {
+        Manager.Instance.Update();
+
+        if ((_enableKeybinding?.Value ?? false) &&
+            (_keybind1.Value != KeyCode.None ? Input.GetKey(_keybind1.Value) : true) &&
+            (_keybind2.Value != KeyCode.None ? Input.GetKeyDown(_keybind2.Value) : true))
+        {
+            UI.Instance.IsWindowOpen = !UI.Instance.IsWindowOpen;
+        }
+    }
+
+    private void InitializeConfigs()
+    {
+        _enableKeybinding = Config.Bind(
+            "Keybinding",
+            "Enable keybinding",
+            true,
+            "Enables or disables keyboard shortcuts to show or hide the GUI"
+        );
+            
+        _keybind1 = Config.Bind(
+            "Keybinding",
+            "Keycode 1",
+            KeyCode.LeftControl,
+            "First keycode."
+        );
+            
+        _keybind2 = Config.Bind(
+            "Keybinding",
+            "Keycode 2",
+            KeyCode.I,
+            "Second keycode.");
+    }
 }
